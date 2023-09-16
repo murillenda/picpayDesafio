55b8737224e5cbf0b967483e5a000c0d87ed1c44
Murillo Marques <giikthelol@gmail.com>
2023-09-16 16:04:27 -0300

    feat: initial config

---
+++ src/main/java/com/picpaydesafio/PicpaydesafioApplication.java
@@ -0,0 +1,13 @@
+package com.picpaydesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaydesafioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicpaydesafioApplication.class, args);
+    }
+
+}
